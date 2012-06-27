6b05fe3cc6d513f23f5dfba4ed304d8013873c93
eriksk <erikskoglund88@gmail.com>
2012-06-27 19:56:52 +0200

    removed duplicate files

--- entity.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'kawaii'
-require_relative 'node'
-
-class Kawaii::Entity < Kawaii::Node
-  
-  def initialize
-  end
-end
--- kawaii.rb
+++
@@ -1,4 +0,0 @@
-module Kawaii
-  # TODO: put helper functions here so that Kawaii::to_radiens ex. will work. looks nice.,
-  
-end
