a3aa3738b176b7ea5b42284d945fbf53046fdf08
eriksk <erikskoglund88@gmail.com>
2012-07-14 11:06:21 +0200

    added commands to generate game

--- lib/kawaii/command.rb
+++ lib/kawaii/command.rb
@@ -1,9 +1,26 @@
+class String
+	# colorization
+	def colorize(text, color_code)
+	  "\e[#{color_code}m#{text}\e[0m"
+	end
+
+	def red
+		colorize(self, 31)
+	end
+	def green
+		colorize(self, 32)
+	end
+	def yellow
+		colorize(self, 33)
+	end
+	def pink
+		colorize(self, 35)
+	end
+end
+
 module Kawaii
 	class Command
 		def self.run command, args
-			puts "Command: #{command}"
-			puts "args: #{args}"
-
 			puts ""
 			case command
 				when "new"
@@ -17,19 +34,62 @@ module Kawaii
 
 		private
 			def self.new_game_template args
-				puts "Kawaii used 'create new game template'"
-				puts "But it failed..."
-				puts "It wasn't implemented yet"	
+				width = 800
+				height = 600
+				fullscreen = false
+				game_class_name = args.first.capitalize 
+				File.open(args.first + ".rb", "w+") do |f|
+					f.write <<-EOF
+require 'kawaii'
+
+class #{game_class_name} < Kawaii::Game
+  
+  WIDTH = #{width}
+  HEIGHT = #{height}
+  FULLSCREEN = #{fullscreen}
+  CONTENT_ROOT = "content"
+  
+  def initialize
+    super WIDTH, HEIGHT, FULLSCREEN, CONTENT_ROOT
+    load_game
+  end
+  
+  def load_game
+  	# TODO: load code goes here	
+  end
+
+  def input
+  	# TODO: input goes here
+    if button_down?Gosu::KbEscape
+      exit
+    end  
+  end
+
+  def before_update
+  	# TODO: game logic goes here
+    input
+  end
+
+  def after_update
+  	# TODO: game logic goes here	
+  end
+end
+
+# entry point
+game = #{game_class_name}.new
+game.show
+				EOF
+				end
+
+				puts "Successfully created new game '#{game_class_name}'".green
+				puts "Run it with the command: " + "ruby #{args.first}.rb".yellow
 			end
 
 			def self.print_help
+				puts "Kawaii <3".pink
 				puts <<-EOF
-Kawaii <3
-
 Commands:
-
-# create new game template
-new [name, width(int), height(int), fullscreen(boolean)]
+	new [name] # create new game template
 				EOF
 			end
 	end
